0fb3544d8a07b05e593da6bda8f6ed318918b778
chambial10752@gmail.com <chambial10752@gmail.com>
2023-12-07 18:51:04 +0000

    Edited settings and sportsform

--- client_code/sportsform/__init__.py
+++ client_code/sportsform/__init__.py
@@ -1,5 +1,8 @@
 from ._anvil_designer import sportsformTemplate
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
 
 class sportsform(sportsformTemplate):
   def __init__(self, **properties):
